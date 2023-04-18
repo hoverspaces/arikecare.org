9cc55ea421432d99f0adf674c387c9d1f69ddefc
Gigin George <mail.gigin@gmail.com>
2023-04-18 20:22:51 +0530

    Cloudflared Deploy Config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+// Config to allow deployment on Cloudflare Pages
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    runtime: 'edge',
+  }
 }
 
 module.exports = nextConfig
